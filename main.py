40e908e7dc72d9b7ff1c21c945e9c9b75e55ce53
Ewann Postic <63552544+Epwo@users.noreply.github.com>
2024-01-24 15:08:38 +0100

    class player

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 # let's start this !
 class Game:
     def __init__(self, nb_players=2):
-        self._board = Board()
+        #self._board = Board()
         self.nbJoueurs = nb_players
-        self._player = Player()
         self.isGameOver = False
         self._piles = {'pile1': [], 'pile2': [], 'pile3': [], 'pile4': []}
         self._dictScore = {}
         self._ReadyList = []
+        self._players = []
         for i in range(nb_players):
-            self._dictScore['J' + str(i)] = 0
+            self._players.append(Player('J' + str(i)))
             # On va mettre dans cette liste les cartes de joueurs qui ont validé leur carte
-            # de telle sorte que la carte joué de Joueur 1 (-> J1-1): self._ReadyList[0] = *numero de la carte*
+            # de telle sorte que la carte joué de Joueur 0 (-> J0): self._ReadyList[0] = *numero de la carte*
             self._ReadyList.append(0)
 
     def validCarte(self, carte, joueur):
         self._ReadyList[joueur - 1] = carte
 
-    def CleanPile(self, pile,player):
+    def CleanPile(self, pile, player):
         pile = pile[-1]
         return pile
 
@@ -32,7 +32,7 @@ class Game:
         for e in self._ReadyList:
             if e != 0:
                 nb += 1
-        if self.nbJoueurs * (2/3) <= nb:
+        if self.nbJoueurs * (2 / 3) <= nb:
             # plus de 2/3 des joueurs ont validé leur carte
             DisplayClock()
         elif self.nbJoueurs == nb:
@@ -41,5 +41,39 @@ class Game:
 
     def play(self):
         # tri dans l'ordre croissant des cartes
-
         self._board.print()
+
+class Player(Game):
+    def __init__(self,name):
+        self._cartes = []
+        self._score = 0
+        self._name = name
+        self._isReady = False
+
+    def setCartes(self, cartes):
+        self._cartes = cartes
+
+    def setScore(self, score):
+        self._score = score
+
+    def setName(self, name):
+        self._name = name
+
+    def setIsReady(self, isReady):
+        self._isReady = isReady
+
+    def getCartes(self):
+        return self._cartes
+
+    def getScore(self):
+        return self._score
+
+    def getName(self):
+        return self._name
+
+    def getIsReady(self):
+        return self._isReady
+
+
+G = Game(nb_players=4)
+G.play()
